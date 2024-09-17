2b4ae9be5d98915ff3b48df1edc5e48b33a941b2
Alexander <107767827+entgorbunov@users.noreply.github.com>
2024-09-17 18:19:31 +0500

    Реализована генерация короткой ссылки и ее просмотр в логах

--- src/main/java/com/panyukovnn/linkShortener/Main.java
+++ src/main/java/com/panyukovnn/linkShortener/Main.java
@@ -3,9 +3,17 @@ package com.panyukovnn.linkShortener;
 import com.panyukovnn.linkShortener.dto.CreateShortLinkRequest;
 import com.panyukovnn.linkShortener.service.LinkService;
 
+import java.time.ZonedDateTime;
+
 public class Main {
     public static void main(String[] args) {
         LinkService linkService = new LinkService();
-        String s = linkService.generateShortLink(new CreateShortLinkRequest());
+        String s = linkService.generateShortLink(new CreateShortLinkRequest(
+                "https://github.com/entgorbunov/link-shortener",
+                ZonedDateTime.now().plusDays(7),
+                "GitHub",
+                true
+        ));
+        System.out.println(s);
     }
 }
