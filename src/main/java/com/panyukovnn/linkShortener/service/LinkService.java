639023ca4e82eab8d3e815905f56ce3ec3e96128
Alexander <107767827+entgorbunov@users.noreply.github.com>
2024-09-18 11:08:20 +0500

    Добавил в класс Constants.java константу =)

--- src/main/java/com/panyukovnn/linkShortener/service/LinkService.java
+++ src/main/java/com/panyukovnn/linkShortener/service/LinkService.java
@@ -1,6 +1,7 @@
 package com.panyukovnn.linkShortener.service;
 
 import com.panyukovnn.linkShortener.dto.CreateShortLinkRequest;
+import com.panyukovnn.linkShortener.util.Constants;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.HashMap;
@@ -8,11 +9,11 @@ import java.util.Map;
 
 public class LinkService {
 
-    public static final int COUNT = 8;
+
     private final Map<String, CreateShortLinkRequest> linkStorage = new HashMap<>();
 
     public String generateShortLink(CreateShortLinkRequest createShortLinkRequest) {
-        String shortLink = RandomStringUtils.randomAlphanumeric(COUNT);
+        String shortLink = RandomStringUtils.randomAlphanumeric(Constants.COUNT);
 
         linkStorage.put(shortLink, createShortLinkRequest);
 
