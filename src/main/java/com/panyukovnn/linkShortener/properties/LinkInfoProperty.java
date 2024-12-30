396bdcd66fac71ac991a26350712cb37cd1146a2
Alexander <107767827+entgorbunov@users.noreply.github.com>
2024-12-30 10:07:26 +0500

    Delete src/main/java/com/panyukovnn/linkShortener directory

--- src/main/java/com/panyukovnn/linkShortener/properties/LinkInfoProperty.java
+++
@@ -1,7 +0,0 @@
-package com.panyukovnn.linkshortener.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "link-shortener")
-public record LinkInfoProperty(Integer shortLinkLength) {
-}
