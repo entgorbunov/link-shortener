3f02d8bd10e568d3f196d59980dd29fe9046a59e
Alexander Gorbunov <entgorbunov@gmail.com>
2025-01-09 15:23:42 +0500

    добавьте Dockerfile в link-shortener собертие контейнер, проверьте что приложение работает добавьте docker-compose.yml файл для link-shortener'а, со своей бд, запустите контейнеры, проверьте что приложение работает

--- src/main/java/com/panyukovnn/linkshortener/config/LinkShortenerConfig.java
+++ src/main/java/com/panyukovnn/linkshortener/config/LinkShortenerConfig.java
@@ -2,18 +2,21 @@ package com.panyukovnn.linkshortener.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class LinkShortenerConfig {
 
     @Bean
     public String notFoundPage() throws IOException {
-        File file = ResourceUtils.getFile("classpath:templates/404.html");
-        return Files.readString(file.toPath());
+        try (InputStream is = new ClassPathResource("templates/404.html").getInputStream()) {
+            byte[] binaryData = FileCopyUtils.copyToByteArray(is);
+            return new String(binaryData, StandardCharsets.UTF_8);
+        }
     }
 }
